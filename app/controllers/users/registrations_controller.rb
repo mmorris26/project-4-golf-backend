53b131eb088b6da07be5c0940b5f88d869ad1618
Miles Morris <milesmorris123@gmail.com>
2023-06-02 12:45:22 +0100

    added functionality to be able to read the auth header in the frontend

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   private
 
+  
+
   def respond_with(resource, _opts = {})
+  
     if request.method == "POST" && resource.persisted?
       render json: {
         status: {code: 200, message: "Signed up sucessfully."},
@@ -21,6 +24,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
       }, status: :unprocessable_entity
     end
   end
+
+  def configure_sign_up_params()
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module GolfBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete]
+         resource '*', :headers => :any, 
+         expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+         :methods => [:get, :post, :patch, :put, :delete]
        end
     end
 
