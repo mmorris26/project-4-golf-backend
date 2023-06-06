25c23c966bc8a63d2638caba2f761d4eed08d91a
Miles Morris <milesmorris123@gmail.com>
2023-06-06 14:52:19 +0100

    adding secret key base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  Devise.secret_key = Rails.application.credentials.fetch(:secret_key_base)
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
