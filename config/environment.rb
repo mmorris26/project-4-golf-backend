47dbcd92e544adcd5205620e4fc4196fb522b867
Miles Morris <milesmorris123@gmail.com>
2023-06-06 15:28:19 +0100

    changing credentials file and mast key file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Devise.secret_key = Rails.application.credentials.fetch(:secret_key_base)
+
 # Initialize the Rails application.
 Rails.application.initialize!
