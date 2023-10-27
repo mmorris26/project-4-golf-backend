8d94134de582da64029ea8ce8067c7ec71502ba0
Miles Morris <milesmorris123@gmail.com>
2023-10-27 10:40:41 +0100

    changing root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,25 @@
 Rails.application.routes.draw do
   get 'current_user/index'
-  # devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 
-  Rails.application.routes.draw do
-    get '/current_user', to: 'current_user#index'
-    devise_for :users, path: '', path_names: {
-      sign_in: 'login',
-      sign_out: 'logout',
-      registration: 'signup'
-    },
-    controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-    }
-  end
+  root 'current_user#index'
+
+  get '/current_user', to: 'current_user#index'
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   get 'courses/current_course', to: "courses#current_course"
-
   get 'rounds/current_round', to: "rounds#current_round"
-
   get 'courses/rounds', to: "courses#course_rounds"
 
   resources :courses
   resources :rounds
-
 end
